d7400ada1ef058dad29f31312ae881f6c77422c1
ahmed <ahmed@AHMAD>
2024-06-28 19:43:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿public sealed class Singleton
+{
+    private static Singleton _instance;
+    private static readonly object LockObject = new object();
+
+    private Singleton()
+    {
+        // Private constructor to prevent external instantiation.
+    }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (LockObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public void SomeMethod()
+    {
+        Console.WriteLine("SomeMethod called.");
+    }
+}
+
+public class Program
+{
+    public static void Main() {
+        // Usage:
+        var singletonInstance = Singleton.Instance;
+        singletonInstance.SomeMethod();
+    }
+
+}
